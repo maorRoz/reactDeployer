3fc00376db801b5b3fe523044da6dbe51d1d3989
maorroz <andory@post.bgu.ac.il>
2018-12-29 01:04:53 +0200

    added the react app

---
+++ build/precache-manifest.ee3eb8840fe1dea3ecf63b562dc0d36a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "499cd5e737ebda8436c3",
+    "url": "/static/js/main.499cd5e7.chunk.js"
+  },
+  {
+    "revision": "1f366176f1665ef7b583",
+    "url": "/static/js/1.1f366176.chunk.js"
+  },
+  {
+    "revision": "499cd5e737ebda8436c3",
+    "url": "/static/css/main.d2eb5215.chunk.css"
+  },
+  {
+    "revision": "a8ec0675667d0ebbf756b3de0799a52f",
+    "url": "/index.html"
+  }
+];
