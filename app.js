1119b194f9b8aac87075028ef0b583a2deab4780
maorroz <andory@post.bgu.ac.il>
2018-12-29 02:37:34 +0200

    fixed print

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+    if(req.url === '/'){
+        console.log('sent page!');
+    }
+    next();
+})
 app.use(express.static(path.join(__dirname, 'build')));
 
+
 app.get('/*', (req, res) => {
-    console.log('sent page!');
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
